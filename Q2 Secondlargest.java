851d980668bee278f309236884c8389bda4e6601
mohithat2006-png <mohitha.t2006@gmail.com>
2026-08-10 08:55:03 +0530

    Add files via upload

---
+++ Q2 Secondlargest.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = arr[0];
+        int second = Integer.MIN_VALUE;
+
+        for (int i = 1; i < n; i++) {
+
+            if (arr[i] > largest) {
+                second = largest;
+                largest = arr[i];
+            }
+            else if (arr[i] > second && arr[i] != largest) {
+                second = arr[i];
+            }
+        }
+
+        if (second == Integer.MIN_VALUE) {
+            System.out.println("Second largest element does not exist");
+        }
+        else {
+            System.out.println("Second Largest = " + second);
+        }
+    }
+}
